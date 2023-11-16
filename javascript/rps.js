81ca0e85f3177dcf461d96c890a71e149e4b5d63
Alexander <139145315+Zaandros@users.noreply.github.com>
2023-11-16 15:39:25 +0000

    added in functionality for buttons to play updated game logic to work with user input from buttons

--- javascript/rps.js
+++ javascript/rps.js
@@ -1,6 +1,102 @@
 
 let playerWins = 0;
 let computerWins = 0;
+let choice = "";
+let gameRoundPlayed = 0;
+
+
+
+function startGame() {
+    console.log(gameRoundPlayed);
+    const ROCK = document.querySelector(".rock");
+    const PAPER = document.querySelector(".paper");
+    const SCISSORS = document.querySelector(".scissors");
+
+
+    ROCK.addEventListener("click", getRock);
+    PAPER.addEventListener("click", getPaper);
+    SCISSORS.addEventListener("click", getScissors);
+}
+
+function getRock() {
+        choice = "rock";
+        game();
+       
+}
+    
+function getPaper() {
+        choice = "paper";
+        game();
+        
+}
+    
+function getScissors() {
+        choice = "scissors";
+        game();
+        
+}
+
+function game() {
+
+
+    do {
+        gameRound();
+        console.log("The rounds played is " + gameRoundPlayed);
+        console.log(`player wins is ${playerWins}`);
+        if (gameRoundPlayed == 5 && playerWins > computerWins) {
+        alert(`You win by ${playerWins} to ${computerWins}`);
+        break;
+    }
+    else if (gameRoundPlayed == 5 && playerWins < computerWins) {
+        alert(`You lose by ${computerWins} to ${playerWins}`);
+        break;
+    }
+    else if (gameRoundPlayed == 5 && playerWins == computerWins) {
+        alert("The game has ended in a draw");
+        break;
+    }
+    else if (gameRoundPlayed < 5) {
+        startGame();
+        break;
+    }
+
+    } while (gameRoundPlayed <= 5)
+    
+}
+
+function gameRound() {
+
+let computerChoice = getComputerChoice();
+let userChoice = getPlayerChoice();
+
+
+    if (userChoice == computerChoice) {
+        alert("The game is a draw");
+        gameRoundPlayed++
+    }
+    else if (userChoice == "rock" && computerChoice == "scissors") {
+        alert(`You win! ${userChoice} beats ${computerChoice}`);
+        gameRoundPlayed++
+        playerWins++;
+    }
+    else if (userChoice == "paper" && computerChoice == "rock") {
+        alert(`You win! ${userChoice} beats ${computerChoice}`);
+        gameRoundPlayed++
+        playerWins++;
+    }
+    else if (userChoice == "scissors" && computerChoice == "paper") {
+        alert(`You win! ${userChoice} beats ${computerChoice}`);
+        gameRoundPlayed++
+        playerWins++;
+    }
+    else {
+        alert(`You lose! ${computerChoice} beats ${userChoice}`);
+        gameRoundPlayed++
+        computerWins++;
+    }
+   
+    return playerWins && computerWins;
+}
 
 function getComputerChoice() {
    
@@ -22,82 +118,43 @@ function getComputerChoice() {
 
 function getPlayerChoice() {  
 
-    let correct = 0;
-    let choice = "";
     
+    let correct = 0;   
+
     do {
-    choice = prompt("Please enter rock, paper, or scissors").toLowerCase();
-    switch (choice) {
-        case "rock":
-            correct = correct++;
-            return choice;
-            break;
-        case "paper":
-            correct = correct++;
-            return choice;
-            break;
-        case "scissors":
-            correct = correct++;
-            return choice;
-            break;
-        default:
-            alert("You have not entered a correct option");
-            break;
-    }
 
-    } while (correct < 1);
+        switch (choice) {
+            case "rock":
+                correct++
+                return choice;
+                break;
+            case "paper":
+                correct++
+                return choice;
+                break;
+            case "scissors":
+                correct++
+                return choice;
+                break;
+            default:
+                alert("You have not pressed a button");
+                break;
+        }
+
     
+    } while (correct < 1)
+
+        
 }
 
-function gameRound() {
 
-let computerChoice = getComputerChoice();
-let userChoice = getPlayerChoice();
-;
+startGame();
 
-    if (userChoice == computerChoice) {
-        alert("The game is a draw");
-    }
-    else if (userChoice == "rock" && computerChoice == "scissors") {
-        alert(`You win! ${userChoice} beats ${computerChoice}`);
-        playerWins++;
-    }
-    else if (userChoice == "paper" && computerChoice == "rock") {
-        alert(`You win! ${userChoice} beats ${computerChoice}`);
-        playerWins++;
-    }
-    else if (userChoice == "scissors" && computerChoice == "paper") {
-        alert(`You win! ${userChoice} beats ${computerChoice}`);
-        playerWins++;
-    }
-    else {
-        alert(`You lose! ${computerChoice} beats ${userChoice}`);
-        computerWins++;
-    }
-    return playerWins && computerWins;
-}
 
-function game() {
 
-    let gameRoundPlayed = 1;
 
-    for (gameRoundPlayed = 1; gameRoundPlayed <= 5; gameRoundPlayed++) {
-        gameRound();
-        console.log(gameRoundPlayed);
-        console.log(`player wins is ${playerWins}`);
-        if (gameRoundPlayed == 5 && playerWins > computerWins) {
-        alert(`You win by ${playerWins} to ${computerWins}`);
-    }
-    else if (gameRoundPlayed == 5 && playerWins < computerWins) {
-        alert(`You lose by ${computerWins} to ${playerWins}`);
-    }
-    else if (gameRoundPlayed == 5 && playerWins == computerWins) {
-        alert("The game has ended in a draw");
-    }
-    }
-    
-    
-}
 
-game();
-    
+ 
+
+
+
